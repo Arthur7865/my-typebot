7e7af52fc860a41a9fe4b1cf65c0c58baaeaa053
Arthur7865 <arthurmelloferreira@gmail.com>
2024-07-06 14:03:13 -0300

    Initial commit

--- index.js
+++
@@ -1,9 +0,0 @@
-echo const express = require('express');^
-const app = express();^
-const port = 3000;^
-app.get('/', (req, res) => {^
-  res.send('Hello World!');^
-});^
-app.listen(port, () => {^
-  console.log(`App running at http://localhost:${port}`);^
-}); > index.js
